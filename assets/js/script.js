ff6b016ea02c4f6357c165dd942fdbca9d4a98b3
wbruns <wbruns07@gmail.com>
2022-01-11 19:24:04 -0600

    changed event listener to submit(form) and added preventDefault()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+// prevents the page from reloading
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+// submit event is form specific, activates when enter is pressed or when a 
+// button with type="submit" within the form is pressed
+formEl.addEventListener("submit", createTaskHandler);
