76cdbac3cca1d12af2de3137937c66df28296133
wbruns <wbruns07@gmail.com>
2022-01-11 19:59:01 -0600

    createTaskHandler() now accepts taskName and taskType, adds them to h3 and span elements respectively, which are nested inside a div and li item, and appends it to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,21 @@ var createTaskHandler = function(event) {
 // prevents the page from reloading
   event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  // add entire li item to ul
   tasksToDoEl.appendChild(listItemEl);
 };
 
